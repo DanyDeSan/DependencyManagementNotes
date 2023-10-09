94cfa4e3d822544f1734263522343f1c3815331a
Dany De San <ldanieldesanpedro@gmail.com>
2023-10-09 13:05:59 -0600

    Add project with cocoapods

---
+++ CocoapodsExercise/RectangleArea/RectangleArea/RectangleAreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectangleAreaApp.swift
+//  RectangleArea
+//
+//  Created by L Daniel De San Pedro on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RectangleAreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CocoapodsExercise/RectangleArea/RectangleArea/RectangleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RectangleViewModel.swift
+//  RectangleArea
+//
+//  Created by L Daniel De San Pedro on 09/10/23.
+//
+
+import Foundation
+import ShapeAreas
+
+final class RectangleViewModel {
+    var calculator: ShapeAreaCalculator = ShapeAreaCalculator()
+}
