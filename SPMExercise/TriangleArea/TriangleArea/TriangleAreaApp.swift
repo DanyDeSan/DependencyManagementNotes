9502ffc27ef36bb421a8d947ee939edd911fbdfe
Dany De San <ldanieldesanpedro@gmail.com>
2023-10-09 16:14:32 -0600

    Add starter triangle area

---
+++ SPMExercise/TriangleArea/TriangleArea/TriangleAreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriangleAreaApp.swift
+//  TriangleArea
+//
+//  Created by L Daniel De San Pedro on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TriangleAreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
