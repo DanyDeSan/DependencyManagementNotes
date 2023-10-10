95c6fddab8a36c6b45078853bd68a03913befeb1
Dany De San <ldanieldesanpedro@gmail.com>
2023-10-09 23:23:47 -0600

    Left starter version of Carthage exercise

--- CarthageExercise/CircleArea/CircleArea/CircleViewModel.swift
+++ CarthageExercise/CircleArea/CircleArea/CircleViewModel.swift
@@ -6,15 +6,13 @@
 //
 
 import Foundation
-import ShapeAreas
+
 
 class CircleViewModel: ObservableObject {
     
     @Published var resultArea: Double = 0
-    
-    let calculator = ShapeAreaCalculator()
-    
+        
     func caculateArea(radius: CGFloat) {
-        resultArea = calculator.circleArea(radius: Double(radius))
+        
     }
 }
