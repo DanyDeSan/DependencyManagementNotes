37182718c12e85e735474f8895e200c30202eb61
Dany De San <ldanieldesanpedro@gmail.com>
2023-10-12 22:05:16 -0600

    Add finished rectangle area

--- CocoapodsExercise/RectangleArea/RectangleArea/RectangleViewModel.swift
+++ CocoapodsExercise/RectangleArea/RectangleArea/RectangleViewModel.swift
@@ -6,12 +6,14 @@
 //
 
 import Foundation
+import ShapeArea
 
 final class RectangleViewModel: ObservableObject {
     
     @Published var rectangleArea: Double = 0
+    var calculator = ShapeAreaCalculator()
 
     func calculateRectangleArea(width: CGFloat, height: CGFloat) {
-        
+        rectangleArea = calculator.rectangularArea(width: Double(width), height: Double(height))
     }
 }
